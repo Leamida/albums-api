21f3d8588c9a05c047eaa5b1641907a2175886c5
leamida <leoanangmh@gmail.com>
2022-07-04 21:38:55 +0700

    Add migration

--- migrations/1656944628639_create-table-songs.js
+++ migrations/1656944628639_create-table-songs.js
@@ -2,6 +2,39 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'NUMBER',
+      notNull: false,
+    },
+    albumId: {
+      type: 'TEXT',
+      notNull: false,
+    },
+  });
+};
 
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
